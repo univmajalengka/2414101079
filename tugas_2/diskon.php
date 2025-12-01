608a071b9562c0286e806e60530ccf7fd104c018
RifalFauzi <rifalfauzi563@gmail.com>
2025-12-02 00:00:52 +0700

    Add files via upload

---
+++ tugas_2/diskon.php
@@ -0,0 +1,23 @@
+<?php
+
+function hitungDiskon($totalBelanja)
+{
+    if ($totalBelanja >= 100000) {
+        $diskon = $totalBelanja * 0.10;
+    } elseif ($totalBelanja >= 50000 && $totalBelanja < 100000) {
+        $diskon = $totalBelanja * 0.05;
+    } else {
+        $diskon = 0;
+    }
+
+    return $diskon;
+}
+
+$totalBelanja = 220000;
+
+$diskon = hitungDiskon($totalBelanja);
+$totalBayar = $totalBelanja - $diskon;
+
+echo "Total Belanja : Rp " . number_format($totalBelanja, 0, ',', '.') . PHP_EOL;
+echo "Diskon        : Rp " . number_format($diskon, 0, ',', '.') . PHP_EOL;
+echo "Total Bayar   : Rp " . number_format($totalBayar, 0, ',', '.');
