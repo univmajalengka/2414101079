549da27dad554a67874f837248f6e66d044a8d8e
RifalFauzi <rifalfauzi563@gmail.com>
2025-12-25 11:33:40 +0700

    diskon.php

--- tugas_2/diskon.php
+++ tugas_2/diskon.php
@@ -1,23 +1,48 @@
 <?php
-
 function hitungDiskon($totalBelanja)
 {
     if ($totalBelanja >= 100000) {
-        $diskon = $totalBelanja * 0.10;
+        return $totalBelanja * 0.10;
     } elseif ($totalBelanja >= 50000 && $totalBelanja < 100000) {
-        $diskon = $totalBelanja * 0.05;
+        return $totalBelanja * 0.05;
     } else {
-        $diskon = 0;
+        return 0;
     }
-
-    return $diskon;
 }
 
-$totalBelanja = 220000;
+$totalBelanja = 0;
+$diskon = 0;
+$totalBayar = 0;
 
-$diskon = hitungDiskon($totalBelanja);
-$totalBayar = $totalBelanja - $diskon;
+if (isset($_POST['totalBelanja'])) {
+    $totalBelanja = (int) $_POST['totalBelanja'];
+    $diskon = hitungDiskon($totalBelanja);
+    $totalBayar = $totalBelanja - $diskon;
+}
+?>
 
-echo "Total Belanja : Rp " . number_format($totalBelanja, 0, ',', '.') . PHP_EOL;
-echo "Diskon        : Rp " . number_format($diskon, 0, ',', '.') . PHP_EOL;
-echo "Total Bayar   : Rp " . number_format($totalBayar, 0, ',', '.');
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Perhitungan Diskon</title>
+</head>
+<body>
+
+<h3>Input Total Belanja</h3>
+
+<form method="post">
+    <label>Total Belanja (Rp): </label><br>
+    <input type="number" name="totalBelanja" required>
+    <br><br>
+    <button type="submit">Hitung</button>
+</form>
+
+<?php if ($totalBelanja > 0): ?>
+    <hr>
+    Total Belanja : Rp <?= number_format($totalBelanja, 0, ',', '.') ?><br>
+    Diskon : Rp <?= number_format($diskon, 0, ',', '.') ?><br>
+    Total Bayar : Rp <?= number_format($totalBayar, 0, ',', '.') ?>
+<?php endif; ?>
+
+</body>
+</html>
